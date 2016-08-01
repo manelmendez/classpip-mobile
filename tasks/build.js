a5024fe413854698e578ba68e496cc7e92bbea0b
Ferran Vila Conesa <ikocel1@gmail.com>
2016-08-01 20:42:15 +0200

    Separate gulp tasks. Close #10

---
+++ tasks/build.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var buildBrowserify = require('ionic-gulp-browserify-typescript');
+var runSequence = require('run-sequence');
+
+var argv = process.argv;
+var isRelease = argv.indexOf('--release') > -1;
+
+gulp.task('build', ['clean'], function(done) {
+  runSequence(
+    ['tslint', 'eslint', 'sass', 'html', 'fonts', 'scripts'],
+    function() {
+      buildBrowserify({
+        minify: isRelease,
+        browserifyOptions: {
+          debug: !isRelease
+        },
+        uglifyOptions: {
+          mangle: false
+        }
+      }).on('end', done);
+    }
+  );
+});
---
+++ tasks/html.js
@@ -0,0 +1,4 @@
+var gulp = require('gulp');
+var copyHTML = require('ionic-gulp-html-copy');
+
+gulp.task('html', copyHTML);
