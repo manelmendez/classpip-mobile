2fea1cebdba79bf63109c8a9fc54b01679926b8b
Ferran Vila Conesa <ikocel1@gmail.com>
2016-08-24 07:40:18 +0200

    Test. Initial implementation

---
+++ app/app.spec.ts
@@ -0,0 +1,33 @@
+import {
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+}                               from '@angular/platform-browser-dynamic/testing';
+import { setBaseTestProviders } from '@angular/core/testing';
+import { MyApp }                from './app';
+
+// this needs doing _once_ for the entire test suite, hence it's here
+setBaseTestProviders(TEST_BROWSER_DYNAMIC_PLATFORM_PROVIDERS,
+  TEST_BROWSER_DYNAMIC_APPLICATION_PROVIDERS);
+
+// Mock out Ionic's platform class
+class MockClass {
+  public ready(): any {
+    return new Promise((resolve: Function) => {
+      resolve();
+    });
+  }
+}
+
+let myApp = null;
+
+describe('MyApp', () => {
+
+  beforeEach(function() {
+    let mockClass = (<any>new MockClass());
+    myApp = new MyApp(mockClass, mockClass);
+  });
+
+  it('initialises with two possible pages', () => {
+    expect(myApp['pages'].length).toEqual(2);
+  });
+});
