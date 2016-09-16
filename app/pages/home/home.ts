e4bc2153952a4a78b98b3d7cedc6d2fd91d4ee62
Vila Conesa, Ferran <Ferran.Vila@gft.com>
2016-09-16 11:35:32 +0200

    integrate utils library. closes #28 and closes #29

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { SampleService, SamplePipe, SampleComponent } from 'classpip-utils'
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
+  directives: [SampleComponent],
+  pipes: [SamplePipe],
+  providers: [SampleService]
 })
 export class HomePage {
 
-  constructor(private navCtrl: NavController) {
+  constructor(private navCtrl: NavController,
+    sampleService: SampleService) {
 
+    console.log(sampleService.getElements());
   }
 
 }
