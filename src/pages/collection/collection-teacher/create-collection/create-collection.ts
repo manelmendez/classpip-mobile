1ffa940c725912575b9609363a3450536257539d
manelmendez <maneeel93@gmail.com>
2017-09-14 00:29:19 +0200

    functions to get picture from gallery or camera added

--- src/pages/collection/collection-teacher/create-collection/create-collection.ts
+++ src/pages/collection/collection-teacher/create-collection/create-collection.ts
@@ -2,16 +2,24 @@
  * Created by manel on 31/5/17.
  */
 import { Component, ViewChild, ElementRef } from '@angular/core';
-import {MenuController, NavController, NavParams} from 'ionic-angular';
+import {
+  ActionSheetController, Loading, LoadingController, MenuController, NavController, NavParams,
+  Platform, ToastController
+} from 'ionic-angular';
 import { TranslateService } from 'ng2-translate/ng2-translate';
 
 import { IonicService } from '../../providers/ionic.service';
 import { School } from '../../model/school';
 import { CollectionCard } from "../../../../model/collectionCard";
 import { UtilsService } from "../../../../providers/utils.service";
-import { Camera } from 'ionic-native';
+import { Camera } from "@ionic-native/camera";
+import { Transfer, TransferObject } from '@ionic-native/transfer';
+import { File } from "@ionic-native/file";
+import { FilePath } from "@ionic-native/file-path";
 
 declare var google;
+declare var cordova;
+
 
 @Component({
   selector: 'page-create-collection',
@@ -22,26 +30,110 @@ export class CollectionCreate {
   @ViewChild('map') mapElement: ElementRef;
   public collectionCard: CollectionCard = new CollectionCard();
   public base64Image: string;
+  lastImage: string = null;
+  loading: Loading;
 
   constructor(
     public navController: NavController,
     public utilsService: UtilsService,
-    public translateService: TranslateService) {
+    public translateService: TranslateService,
+    private camera: Camera,
+    private transfer: Transfer,
+    private file: File,
+    private filePath: FilePath,
+    public actionSheetCtrl: ActionSheetController,
+    public platform: Platform,
+    public loadingCtrl: LoadingController,
+    public toastCtrl: ToastController) {
 
   }
   public createCollection(): void {
     alert(this.collectionCard.name+' / '+this.collectionCard.num+' / '+this.collectionCard.image);
   }
-  public takePicture(){
-    Camera.getPicture({
-      destinationType: Camera.DestinationType.DATA_URL,
-      targetWidth: 1000,
-      targetHeight: 1000
-    }).then((imageData) => {
-      // imageData is a base64 encoded string
-      this.base64Image = "data:image/jpeg;base64," + imageData;
-    }, (err) => {
-      console.log(err);
+  public presentActionSheet() {
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'Select Image Source',
+      buttons: [
+        {
+          text: 'Load from Library',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+          }
+        },
+        {
+          text: 'Use Camera',
+          handler: () => {
+            this.takePicture(this.camera.PictureSourceType.CAMERA);
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
     });
+    actionSheet.present();
+  }
+  public takePicture(sourceType) {
+    // Create options for the Camera Dialog
+    var options = {
+      quality: 100,
+      sourceType: sourceType,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    };
+
+    // Get the data of an image
+    this.camera.getPicture(options).then((imagePath) => {
+      // Special handling for Android library
+      if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+        this.filePath.resolveNativePath(imagePath)
+          .then(filePath => {
+            let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+            let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+            this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+          });
+      } else {
+        var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+        var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+        this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+      }
+    }, (err) => {
+      this.presentToast('Error while selecting image.');
+    });
+  }
+  // Create a new name for the image
+  private createFileName() {
+    var d = new Date(),
+      n = d.getTime(),
+      newFileName =  n + ".jpg";
+    return newFileName;
+  }
+
+// Copy the image to a local folder
+  private copyFileToLocalDir(namePath, currentName, newFileName) {
+    this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+      this.lastImage = newFileName;
+    }, error => {
+      this.presentToast('Error while storing file.');
+    });
+  }
+
+  private presentToast(text) {
+    let toast = this.toastCtrl.create({
+      message: text,
+      duration: 3000,
+      position: 'top'
+    });
+    toast.present();
+  }
+
+// Always get the accurate path to your apps folder
+  public pathForImage(img) {
+    if (img === null) {
+      return '';
+    } else {
+      return cordova.file.dataDirectory + img;
+    }
   }
 }
